f1c758e146d2d9ac89d3e841dc7951ea415e2c9a
JooYoo <jooyoo@outlook.com>
2023-02-24 14:58:38 +0100

    fix: unit tests after introduced dependency injection

--- test/weekly_date_obj/repo/weely_date_obj_repo_test.dart
+++ test/weekly_date_obj/repo/weely_date_obj_repo_test.dart
@@ -12,12 +12,23 @@ import './weely_date_obj_repo_test.mocks.dart';
 // generate mock to run `flutter pub run build_runner build`
 @GenerateMocks([WeeklyDateObjRemoteDataSourceProtocol])
 void main() {
-  late MockWeeklyDateObjRemoteDataSourceProtocol mockRemoteDataSource;
+  late WeeklyDateObjRemoteDataSourceProtocol remoteDSProtocol;
   late WeeklyDateObjRepo sut;
 
   setUp(() {
-    mockRemoteDataSource = MockWeeklyDateObjRemoteDataSourceProtocol();
+    // Get_it: register the mock-remoteDS to the real-remoteDS
+    locator.registerLazySingleton<WeeklyDateObjRemoteDataSourceProtocol>(
+        () => MockWeeklyDateObjRemoteDataSourceProtocol());
+    // Dependecy injection: get the remoteDS which is now with the mock-remoteDS instance
+    remoteDSProtocol = locator.get<WeeklyDateObjRemoteDataSourceProtocol>();
+
     sut = WeeklyDateObjRepo();
+  });
+
+  tearDown(() {
+    // Unregiester mock after each test case,
+    // so that the new mock can be registered for the next test case.
+    locator.unregister<WeeklyDateObjRemoteDataSourceProtocol>();
   });
 
   test('downloadObjs_whenFunctionIsCalled_thenShouldReturnMockedObjs',
@@ -27,21 +38,25 @@ void main() {
       WeeklyDateObj(fullDate: "2022-1-1", floors: List.empty()),
       WeeklyDateObj(fullDate: "2022-1-2", floors: List.empty())
     ];
+
     // When
-    when(mockRemoteDataSource.downloadObjs())
+    when(remoteDSProtocol.downloadObjs())
         .thenAnswer((_) => Future(() => weeklyDateObjs));
+
+    /// The implementation of `downloadObjs()` is inside of remoteDS,
+    /// it's now mocked, including mocked data.
     var res = await sut.downloadObjs();
+
     // Then
-    verify(mockRemoteDataSource.downloadObjs()).called(1);
+    verify(remoteDSProtocol.downloadObjs()).called(1);
     expect(res.length, 2);
   });
 
   test('deleteObjs_whenFunctionIsCalled_thenCalledOnce', () {
     // When
-    when(mockRemoteDataSource.deleteObjs())
-        .thenAnswer((_) => Future(() => null));
+    when(remoteDSProtocol.deleteObjs()).thenAnswer((_) => Future(() => null));
     sut.deleteObjs();
     // Then
-    verify(mockRemoteDataSource.deleteObjs()).called(1);
+    verify(remoteDSProtocol.deleteObjs()).called(1);
   });
 }
